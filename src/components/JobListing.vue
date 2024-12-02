311160517dbe3e78622ff7c1e0eb9d974f92df96
prana <prana@qtrust.id>
2024-12-02 21:32:06 +0700

    feature: add navbar active links and more views

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -50,10 +50,10 @@ export default {
                     <i class="pi pi-map-marker text-orange-700"></i>
                     {{ job.location }}
                 </div>
-                <a :href="/job/ + `${job.id}`"
+                <RouterLink :to="/jobs/ + `${job.id}`"
                     class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                     Read More
-                </a>
+                </RouterLink>
             </div>
         </div>
     </div>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,6 +1,6 @@
 <script>
 import jobData from "@/jobs.json";
-import JobListing from "./JobListing.vue";
+import JobListing from "@/components/JobListing.vue";
 
 export default {
     data() {
@@ -10,13 +10,6 @@ export default {
     },
     components: {
         JobListing
-    },
-    props: {
-        limit: Number,
-        showButton: {
-            type: Boolean,
-            default: false
-        }
     }
 }
 </script>
@@ -28,13 +21,9 @@ export default {
                 Browse Jobs
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+                <!-- Job Listing 1 -->
+                <JobListing v-for="job in jobs" :key="job.id" :job="job" />
             </div>
         </div>
-    </section>
-
-    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-        <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
-            All Jobs</RouterLink>
     </section>
 </template>
